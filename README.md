90f1d1c991c0f87d08788ed54ffbf376fa10b1cf
George-Seven <uploadtodrive707@gmail.com>
2024-12-27 09:56:09 +0530

    Update

---
+++ README.md
@@ -0,0 +1,121 @@
+# Termux-XEMU
+Trying to natively build Xemu Original Xbox emulator and run it Termux.
+
+Without Root, Proot, Chroot, Box64, Wine.
+
+# Install
+    apt update -y && \
+    yes | apt upgrade -y && \
+    apt install -y --no-install-recommends wget openbox && \
+    wget -O xemu-arm64.deb "https://github.com/George-Seven/Termux-XEMU/releases/latest/download/xemu-arm64.deb" && \
+    apt install ./xemu-arm64.deb
+
+# Use
+1) Download the game's ISO file for the [Original Xbox](https://myrient.erista.me/files/Redump/Microsoft%20-%20Xbox/). If it is zipped/compressed, extract the ISO file from the zip.
+
+2) Convert the ISO file to XISO format needed by Xemu emulator.
+
+       iso2xiso "/path/to/game.iso"
+
+    Replace `"/path/to/game.iso"` with the file path to the game's ISO.
+    
+    Copy the path of the converted file that will be printed, such as `"/path/to/game.x.iso"`
+
+3) Install Termux:X11 -
+
+    [https://github.com/termux/termux-x11/releases/download/nightly/app-universal-debug.apk](https://github.com/termux/termux-x11/releases/download/nightly/app-universal-debug.apk)
+
+4) Start the game in Xemu emulator -
+
+       export DISPLAY=:1 && kill -9 $(pgrep -f "termux.x11") 2>/dev/null; if command -v openbox-session >/dev/null 2>&1; then termux-x11 -ac -xstartup openbox-session :1 2>/dev/null; else termux-x11 -ac :1; fi &
+       
+    And then,
+       
+       xemu -dvd_path "/path/to/game.x.iso"
+    
+    Replace `"/path/to/game.x.iso"` with the game's XISO file path.
+    
+    Open Termux:X11 app to access Xemu emulator UI.
+
+5) Renderer is OpenGL by default. Try switching to Vulkan in **Xemu > View > Backend > Vulkan**.
+
+# Progress
+Seems to be working normally with OpenGL renderer. The games work, but OpenGL renderer is too slow. Instead trying to use Vulkan renderer.
+
+Normally when using Vulkan renderer (in chroot) it gives around 10~12 times more FPS -
+
+[https://www.reddit.com/r/EmulationOnAndroid/comments/1hkxaqj/ninja_gaiden_black_on_android_xemu_original_xbox/](https://www.reddit.com/r/EmulationOnAndroid/comments/1hkxaqj/ninja_gaiden_black_on_android_xemu_original_xbox/)
+
+It works in chroot, so it should work even better without root.
+
+# Vulkan Backend Attempts
+
+For maximum performance, trying to run it natively in Termux using this Vulkan implementation -
+
+**[mesa-vulkan-icd-wrapper](https://github.com/termux/termux-packages/pull/22500)**
+
+It could potentially work with Mali/Xclipse GPU as well -
+
+[https://github.com/xMeM/vulkan-wsi-layer/issues/40](https://github.com/xMeM/vulkan-wsi-layer/issues/40)
+
+But, it is crashing when trying to switch to Vulkan renderer in **Xemu > View > Backend > Vulkan**.
+
+```
+$ apt install -y /sdcard/Download/mesa-vulkan-icd-wrapper_24.3.1-4_aarch64.deb
+$ termux-x11 :1 &
+$ export VK_INSTANCE_LAYERS=VK_LAYER_KHRONOS_validation
+$ DISPLAY=:1 xemu
+xemu_version: 0.7.132-123-g98a03e944c
+xemu_branch: master
+xemu_commit: 98a03e944c5d4c9f3f4973a51bf3b707819d1a89
+xemu_date: Thu Dec 26 07:12:08 UTC 2024
+xemu_settings_get_base_path: base path: /data/data/com.termux/files/home/.local/share/xemu/xemu/
+xemu_settings_get_path: config path: /data/data/com.termux/files/home/.local/share/xemu/xemu/xemu.toml
+CPU: 
+OS_Version: Unknown Distro
+GL_VENDOR: Mesa/X.org
+GL_RENDERER: llvmpipe (LLVM 11.1.0, 128 bits)
+GL_VERSION: 4.5 (Core Profile) Mesa 22.0.5
+GL_SHADING_LANGUAGE_VERSION: 4.50
+Created QEMU launch parameters: xemu -machine xbox,bootrom=/data/data/com.termux/files/usr/share/xemu/mcpx_1.0.bin,kernel-irqchip=off,avpack=hdtv -device smbus-storage,file=/data/data/com.termux/files/home/.local/share/xemu/xemu/eeprom.bin -bios /data/data/com.termux/files/usr/share/xemu/4627v1.03.bin -m 64 -drive index=0,media=disk,file=/data/data/com.termux/files/usr/share/xemu/xbox_hdd.qcow2,locked=on -drive index=1,media=cdrom,file= -display xemu -device usb-hub,port=1,ports=4 
+Enabled instance extensions:
+- VK_KHR_surface
+- VK_KHR_xlib_surface
+- VK_KHR_get_physical_device_properties2
+- VK_KHR_external_semaphore_capabilities
+- VK_KHR_external_memory_capabilities
+Available physical devices:
+- Adreno (TM) 750
+Selected physical device: Adreno (TM) 750
+- Vendor: 5143, Device: 43051401
+- Driver Version: 512.744.12
+Warning: extension not available: VK_EXT_memory_budget
+Enabled device extensions:
+- VK_KHR_external_semaphore
+- VK_KHR_external_memory
+- VK_KHR_external_memory_fd
+- VK_KHR_external_semaphore_fd
+- VK_EXT_custom_border_color
+- VK_EXT_provoking_vertex
+VUID-VkMemoryAllocateInfo-pNext-00639(ERROR / SPEC): msgNum: -49292556 - Validation Error: [ VUID-VkMemoryAllocateInfo-pNext-00639 ] Object 0: handle = 0x84e000000084e, type = VK_OBJECT_TYPE_IMAGE; Object 1: handle = 0x84f000000084f, type = VK_OBJECT_TYPE_DEVICE_MEMORY; | MessageID = 0xfd0fdaf4 | vkBindImageMemory(): memory (VkDeviceMemory 0x84f000000084f[]) has VkExportMemoryAllocateInfo::handleTypes with the VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT flag set, which requires dedicated allocation for the image created with format (VK_FORMAT_R8G8B8A8_UNORM), type (VK_IMAGE_TYPE_2D), tiling (VK_IMAGE_TILING_LINEAR), usage (VK_IMAGE_USAGE_SAMPLED_BIT|VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT), flags (VkImageCreateFlags(0)), but the memory is allocated without dedicated allocation support.
+The Vulkan spec states: If the pNext chain includes a VkExportMemoryAllocateInfo structure, and any of the handle types specified in VkExportMemoryAllocateInfo::handleTypes require a dedicated allocation, as reported by vkGetPhysicalDeviceImageFormatProperties2 in VkExternalImageFormatProperties::externalMemoryProperties.externalMemoryFeatures, or by vkGetPhysicalDeviceExternalBufferProperties in VkExternalBufferProperties::externalMemoryProperties.externalMemoryFeatures, the pNext chain must include a VkMemoryDedicatedAllocateInfo or VkDedicatedAllocationMemoryAllocateInfoNV structure with either its image or buffer member set to a value other than VK_NULL_HANDLE (https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VUID-VkMemoryAllocateInfo-pNext-00639)
+    Objects: 2
+        [0] 0x84e000000084e, type: 10, name: NULL
+        [1] 0x84f000000084f, type: 8, name: NULL
+VUID-VkImageViewCreateInfo-image-01020(ERROR / SPEC): msgNum: -1539087141 - Validation Error: [ VUID-VkImageViewCreateInfo-image-01020 ] Object 0: handle = 0x84e000000084e, type = VK_OBJECT_TYPE_IMAGE; | MessageID = 0xa44364db | vkCreateImageView(): pCreateInfo->image VkImage 0x84e000000084e[] used with no memory bound. Memory should be bound by calling vkBindImageMemory().
+The Vulkan spec states: If image is non-sparse then it must be bound completely and contiguously to a single VkDeviceMemory object (https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VUID-VkImageViewCreateInfo-image-01020)
+    Objects: 1
+        [0] 0x84e000000084e, type: 10, name: NULL
+vk_result = -1
+../hw/xbox/nv2a/pgraph/vk/display.c:691: void create_display_image(PGRAPHState *, int, int): assertion "vk_result == VK_SUCCESS && "vk check failed"" failed
+```
+
+Crashing at line -
+
+**[xemu/hw/xbox/nv2a/pgraph/vk/display.c:691](https://github.com/xemu-project/xemu/blob/98a03e944c5d4c9f3f4973a51bf3b707819d1a89/hw/xbox/nv2a/pgraph/vk/display.c#L691)**
+
+# Build
+    cd; \
+    git clone https://github.com/George-Seven/Termux-XEMU; \
+    cd Termux-XEMU; \
+    ./build-xemu.sh
---
+++ build-xemu.sh
@@ -0,0 +1,362 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+set -e
+
+clear 2>/dev/null || true
+
+apt update -y && \
+yes | apt upgrade -y && \
+yes | apt install -y x11-repo tur-repo && \
+yes | apt install -y git build-essential sdl2 libepoxy libpixman gtk3 libsamplerate libpcap ninja python-pip libslirp binutils pkg-config cmake xorgproto vulkan-headers libglvnd-dev wget ca-certificates termux-x11-nightly bsdtar xz-utils termux-create-package && \
+pip install pyyaml
+
+set -ux
+
+mkdir -p build-xemu
+
+cd build-xemu
+
+CURRENT_DIR="$(pwd)"
+
+rm -rf termux-packages xemu _lib
+
+[ -f .termux-packages.backup ] || { git clone https://github.com/termux/termux-packages termux-packages.backup || { rm -rf termux-packages.backup && exit 1; }; }
+
+touch .termux-packages.backup
+
+(cd termux-packages.backup; git pull || true)
+
+cp -rf termux-packages.backup termux-packages
+
+[ -f .xemu.backup ] || { git clone --recursive https://github.com/xemu-project/xemu xemu.backup || { rm -rf xemu.backup && exit 1; }; }
+
+touch .xemu.backup
+
+cp -rf xemu.backup xemu
+
+(cd xemu; git reset --hard 98a03e944c5d4c9f3f4973a51bf3b707819d1a89)
+
+# Patch 1
+patch -p1 -d xemu <<< '
+--- xemu/util/async-teardown.c	2024-12-25 03:20:11.561540567 +0530
++++ xemu.mod/util/async-teardown.c	2024-12-25 12:14:32.785528336 +0530
+@@ -39,13 +39,13 @@
+     int fd, dfd;
+     DIR *dir;
+ 
+-#ifdef CONFIG_CLOSE_RANGE
+-    int r = close_range(0, ~0U, 0);
+-    if (!r) {
+-        /* Success, no need to try other ways. */
+-        return;
+-    }
+-#endif
++//#ifdef CONFIG_CLOSE_RANGE
++//    int r = close_range(0, ~0U, 0);
++//    if (!r) {
++//        /* Success, no need to try other ways. */
++//        return;
++//    }
++//#endif
+ 
+     dir = opendir("/proc/self/fd");
+     if (!dir) {
+'
+
+# Patch 2
+patch -p1 -d xemu <<< '
+--- xemu/crypto/random-platform.c	2024-12-25 03:20:10.865540567 +0530
++++ xemu.mod/crypto/random-platform.c	2024-12-25 12:11:03.397528416 +0530
+@@ -44,14 +44,14 @@
+         return -1;
+     }
+ #else
+-# ifdef CONFIG_GETRANDOM
+-    if (getrandom(NULL, 0, 0) == 0) {
+-        /* Use getrandom() */
+-        fd = -1;
+-        return 0;
+-    }
+-    /* Fall through to /dev/urandom case.  */
+-# endif
++//# ifdef CONFIG_GETRANDOM
++//    if (getrandom(NULL, 0, 0) == 0) {
++//        /* Use getrandom() */
++//        fd = -1;
++//        return 0;
++//    }
++//    /* Fall through to /dev/urandom case.  */
++//# endif
+     fd = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
+     if (fd == -1 && errno == ENOENT) {
+         fd = open("/dev/random", O_RDONLY | O_CLOEXEC);
+@@ -75,24 +75,24 @@
+         return -1;
+     }
+ #else
+-# ifdef CONFIG_GETRANDOM
+-    if (likely(fd < 0)) {
+-        while (1) {
+-            ssize_t got = getrandom(buf, buflen, 0);
+-            if (likely(got == buflen)) {
+-                return 0;
+-            }
+-            if (got >= 0) {
+-                buflen -= got;
+-                buf += got;
+-            } else if (errno != EINTR) {
+-                error_setg_errno(errp, errno, "getrandom");
+-                return -1;
+-            }
+-        }
+-    }
+-    /* Fall through to /dev/urandom case.  */
+-# endif
++//# ifdef CONFIG_GETRANDOM
++//    if (likely(fd < 0)) {
++//        while (1) {
++//            ssize_t got = getrandom(buf, buflen, 0);
++//            if (likely(got == buflen)) {
++//                return 0;
++//            }
++//            if (got >= 0) {
++//                buflen -= got;
++//                buf += got;
++//            } else if (errno != EINTR) {
++//                error_setg_errno(errp, errno, "getrandom");
++//                return -1;
++//            }
++//        }
++//    }
++//    /* Fall through to /dev/urandom case.  */
++//# endif
+     while (1) {
+         ssize_t got = read(fd, buf, buflen);
+         if (likely(got == buflen)) {
+'
+
+# Patch 3
+patch -p1 -d xemu <<< '
+--- xemu/block/file-posix.c	2024-12-25 03:20:10.825540567 +0530
++++ xemu.mod/block/file-posix.c	2024-12-25 19:47:19.981517973 +0530
+@@ -1792,37 +1792,76 @@
+ }
+ #endif
+ 
++//static int handle_aiocb_copy_range(void *opaque)
++//{
++//    RawPosixAIOData *aiocb = opaque;
++//    uint64_t bytes = aiocb->aio_nbytes;
++//    off_t in_off = aiocb->aio_offset;
++//    off_t out_off = aiocb->copy_range.aio_offset2;
++//
++//    while (bytes) {
++//        ssize_t ret = copy_file_range(aiocb->aio_fildes, &in_off,
++//                                      aiocb->copy_range.aio_fd2, &out_off,
++//                                      bytes, 0);
++//        trace_file_copy_file_range(aiocb->bs, aiocb->aio_fildes, in_off,
++//                                   aiocb->copy_range.aio_fd2, out_off, bytes,
++//                                   0, ret);
++//        if (ret == 0) {
++//            /* No progress (e.g. when beyond EOF), let the caller fall back to
++//             * buffer I/O. */
++//            return -ENOSPC;
++//        }
++//        if (ret < 0) {
++//            switch (errno) {
++//            case ENOSYS:
++//                return -ENOTSUP;
++//            case EINTR:
++//                continue;
++//            default:
++//                return -errno;
++//            }
++//        }
++//        bytes -= ret;
++//    }
++//    return 0;
++//}
++
+ static int handle_aiocb_copy_range(void *opaque)
+ {
+     RawPosixAIOData *aiocb = opaque;
+     uint64_t bytes = aiocb->aio_nbytes;
+     off_t in_off = aiocb->aio_offset;
+     off_t out_off = aiocb->copy_range.aio_offset2;
++    char buffer[8192]; // Use a buffer for data transfer.
++
++    while (bytes > 0) {
++        size_t to_read = bytes > sizeof(buffer) ? sizeof(buffer) : bytes;
+ 
+-    while (bytes) {
+-        ssize_t ret = copy_file_range(aiocb->aio_fildes, &in_off,
+-                                      aiocb->copy_range.aio_fd2, &out_off,
+-                                      bytes, 0);
+-        trace_file_copy_file_range(aiocb->bs, aiocb->aio_fildes, in_off,
+-                                   aiocb->copy_range.aio_fd2, out_off, bytes,
+-                                   0, ret);
+-        if (ret == 0) {
+-            /* No progress (e.g. when beyond EOF), let the caller fall back to
+-             * buffer I/O. */
++        ssize_t read_bytes = pread(aiocb->aio_fildes, buffer, to_read, in_off);
++        if (read_bytes < 0) {
++            if (errno == EINTR) {
++                continue; // Retry on interrupted system call.
++            }
++            return -errno; // Return negative errno for other errors.
++        }
++        if (read_bytes == 0) {
++            // Reached EOF, let the caller fall back to buffer I/O.
+             return -ENOSPC;
+         }
+-        if (ret < 0) {
+-            switch (errno) {
+-            case ENOSYS:
+-                return -ENOTSUP;
+-            case EINTR:
+-                continue;
+-            default:
+-                return -errno;
++
++        ssize_t written_bytes = pwrite(aiocb->copy_range.aio_fd2, buffer, read_bytes, out_off);
++        if (written_bytes < 0) {
++            if (errno == EINTR) {
++                continue; // Retry on interrupted system call.
+             }
++            return -errno; // Return negative errno for other errors.
+         }
+-        bytes -= ret;
++
++        in_off += written_bytes;
++        out_off += written_bytes;
++        bytes -= written_bytes;
+     }
++
+     return 0;
+ }
+'
+
+find termux-packages/x11-packages/qemu-system-x86-64 -type f -name "*.patch" -exec sed -i 's#@TERMUX_PREFIX@#/data/data/com.termux/files/usr#g' {} \;
+
+(cd termux-packages/x11-packages/qemu-system-x86-64/setjmp-aarch64; mkdir -p private; for s in $(find . -name "private-*\.h"); do cp ${s} ./$(basename ${s} | sed "s/-/\//g"); done; clang -I. setjmp.S -c; ar cru libandroid-setjmp.a setjmp.o)
+
+_LIB_DIR="$(pwd)/_lib"
+
+mkdir -p "$_LIB_DIR"
+
+mv termux-packages/x11-packages/qemu-system-x86-64/setjmp-aarch64/libandroid-setjmp.a "$_LIB_DIR"
+
+find $(pwd)/termux-packages/x11-packages/qemu-system-x86-64 -type f -name "*.patch" -exec patch -p1 -f -d xemu -i {} \;
+
+(cd termux-packages; git reset --hard a35ea9f5c40e9613cc91af56cc6a15ad9a0cf868)
+
+find $(pwd)/termux-packages/x11-packages/qemu-system-x86-64 -type f -name "*.patch" -exec sed -i 's#@TERMUX_PREFIX@#/data/data/com.termux/files/usr#g' {} \;
+
+patch -p1 -d xemu -i $(pwd)/termux-packages/x11-packages/qemu-system-x86-64/qemu-7.1.0-linux-user-mmap.c.patch
+
+patch -p1 -d xemu -i $(pwd)/termux-packages/x11-packages/qemu-system-x86-64/qemu-7.2.0-linux-user-syscall.c.patch
+
+set +ux
+
+(cd xemu; LDFLAGS=" -landroid-shmem -llog -L$_LIB_DIR -l:libandroid-setjmp.a" ./build.sh)
+
+rm -rf "$CURRENT_DIR/xemu.deb"
+
+TMP_DIR="$(mktemp -d)"; \
+( \
+cd "$TMP_DIR" && \
+wget -O mcpx_1.0.bin "https://archive.org/download/xemustarter/XEMU%20FILES.zip/XEMU%20FILES%2FBoot%20ROM%20Image%2Fmcpx_1.0.bin" && \
+wget -O 4627v1.03.bin "https://archive.org/download/xemustarter/XEMU%20FILES.zip/XEMU%20FILES%2FBIOS%2FComplex_4627v1.03.bin" && \
+wget -O- https://github.com/xemu-project/xemu-hdd-image/releases/latest/download/xbox_hdd.qcow2.zip | bsdtar -xOf - > xbox_hdd.qcow2 && \
+XEMU_VERSION="$("$CURRENT_DIR/xemu/scripts/xemu-version.sh" "$CURRENT_DIR/xemu" | grep " XEMU_VERSION " | sed "s/\"//g" | awk "{print \$NF}")" && \
+echo '
+{
+    "control": {
+        "Package": "xemu",
+        "Version": "'$(cut -d- -f1<<<$XEMU_VERSION)-$(cut -d- -f2<<<$XEMU_VERSION)~$(cut -d- -f3<<<$XEMU_VERSION)'",
+        "Architecture": "aarch64",
+        "Maintainer": "George-Seven",
+        "Pre-Depends": "x11-repo",
+        "Depends": "libpixman, libepoxy, sdl2, libsamplerate, gtk3, zlib, glib, openssl, libslirp, libpcap, libc++, libiconv, libx11, libxext, libxcursor, libxi, libxfixes, libxrandr, libxss, libwayland, libxkbcommon, libdecor, libxcb, libandroid-support, libxau, libxdmcp, libxrender, libffi, pango, harfbuzz, libcairo, fontconfig, fribidi, gdk-pixbuf, at-spi2-core, libxdamage, libxcomposite, libxinerama, libpng, libjpeg-turbo, pcre2, libandroid-shmem, freetype, libexpat, libbz2, brotli, libgraphite, termux-x11-nightly",
+        "Homepage": "https://github.com/George-Seven/Termux-XEMU",
+        "Description": [
+            "A free and open-source emulator for the original Xbox console.",
+            " Supports connecting up to 4 controllers for local play, networking for multiplayer, resolution scaling, and more.",
+            " Note: Controllers do not work directly on Termux:X11 yet. Try keyboards."
+            ]
+    },
+    "data_files": {
+        "bin/xemu": { "source": "'$CURRENT_DIR'/xemu/dist/xemu" },
+        "bin/iso2xiso": { "source": "iso2xiso" },
+        "share/doc/xemu/LICENSE.txt": { "source": "'$CURRENT_DIR'/xemu/dist/LICENSE.txt" },
+        "share/xemu/mcpx_1.0.bin": { "source": "mcpx_1.0.bin" },
+        "share/xemu/4627v1.03.bin": { "source": "4627v1.03.bin" },
+        "share/xemu/xbox_hdd.qcow2": { "source": "xbox_hdd.qcow2" }
+    },
+    "deb_name": "xemu.deb"
+}
+' > manifest.json && \
+echo '
+mkdir -p "$HOME/.local/share/xemu/xemu"
+echo "
+[general]
+show_welcome = false
+
+[display.debug.video]
+x_pos = 1100
+y_pos = 30
+x_winsize = 180
+y_winsize = 60
+
+[sys.files]
+bootrom_path = '\''$PREFIX/share/xemu/mcpx_1.0.bin'\''
+flashrom_path = '\''$PREFIX/share/xemu/4627v1.03.bin'\''
+hdd_path = '\''$PREFIX/share/xemu/xbox_hdd.qcow2'\''
+" > "$HOME/.local/share/xemu/xemu/xemu.toml"
+' > postinst && \
+echo '#!/data/data/com.termux/files/usr/bin/sh
+
+help_text(){
+echo " Usage -"
+echo
+echo "  $(basename "$0") \"/path/to/game.iso\""
+echo
+}
+
+[ -z "$1" ] && help_text && exit 1
+
+ISO_PATH="$(readlink -f "$1")"
+
+set -e
+
+[ -f "$ISO_PATH" ] || { echo && echo "Error: File not found" && echo && help_text && exit 1; }
+
+ISO_NAME="$(basename "$ISO_PATH")"
+
+if ! echo "$ISO_NAME" | grep -Eq "\.x\.iso$"; then
+  dd if="$ISO_PATH" of="$(dirname "$ISO_PATH")/$(echo "$ISO_NAME" | sed -E "s/\.iso$//").x.iso" skip=387 bs=1M
+  echo
+  echo "Saved to -"
+  echo " \"$(dirname "$ISO_PATH")/$(echo "$ISO_NAME" | sed -E "s/\.iso$//").x.iso\""
+  echo
+else
+  echo
+  echo "File seems to be XISO"
+  echo
+fi
+' > iso2xiso && \
+chmod 755 iso2xiso && \
+termux-create-package manifest.json && \
+mv xemu.deb "$CURRENT_DIR" \
+); \
+rm -rf "$TMP_DIR"
+
+cd "$CURRENT_DIR"
+
+apt install -y "$CURRENT_DIR/xemu.deb"
+
+echo
+echo "Output debian package -"
+echo
+echo " \"$CURRENT_DIR/xemu.deb\""
+echo
