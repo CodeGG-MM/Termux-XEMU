40bd4286da137575fd761e73029db4091f494bac
George-Seven <92206927+George-Seven@users.noreply.github.com>
2025-01-13 10:26:55 +0530

    Update build-xemu.sh

--- build-xemu.sh
+++ build-xemu.sh
@@ -24,13 +24,13 @@ cd termux-packages
 
 ./scripts/setup-termux.sh
 
-yes | apt install -y gtk3 libepoxy libglvnd-dev libpcap libpixman libsamplerate libslirp libtasn1 ndk-sysroot sdl2 vulkan-headers xorgproto
+#yes | apt install -y gtk3 libepoxy libglvnd-dev libpcap libpixman libsamplerate libslirp libtasn1 ndk-sysroot sdl2 vulkan-headers xorgproto
 
 set +u
 [ -z "$LD_PRELOAD" ] && LD_PRELOAD=/data/data/com.termux/files/usr/lib/libtermux-exec.so
 
 set -u
-LD_PRELOAD="$LD_PRELOAD" ./build-package.sh -f -s xemu
+LD_PRELOAD="$LD_PRELOAD" ./build-package.sh -f -I xemu
 
 rm -rf "$CURRENT_DIR/xemu.deb"
 
